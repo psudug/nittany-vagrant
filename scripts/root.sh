7a02fc24a8fa3a56f73d5f307311a1fa3f549af5
btopro <bto108@psu.edu>
2015-03-11 16:54:17 -0400

    root file was duplicated

--- scripts/root.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#call the first bash script
-bash /vagrant/scripts/server/install-system-dependencies.sh
-# apply performance
-bash /vagrant/scripts/server/install-system-performance.sh
-chown vagrant:root /var/www/html
-chmod 775 /var/www/html
