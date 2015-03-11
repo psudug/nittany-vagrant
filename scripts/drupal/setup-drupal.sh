b64f7fa0bf643da74e28a9f0e28e4d63284ad3d7
btopro <bto108@psu.edu>
2015-03-11 12:04:20 -0400

    setup drupal 7 and admin password

--- scripts/drupal/setup-drupal.sh
+++ scripts/drupal/setup-drupal.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/sh
 cd /var/www/html/
 # download drupal
-drush dl drupal --drupal-project-rename=nittany
+drush dl drupal-7 --drupal-project-rename=nittany
 # do the initial drush si so we have minimal in place
 drush si minimal --db-url=mysql://root:@localhost/nittany --account-name=admin --account-mail=admin@nittany.psudug.dev install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --root=/var/www/html/nittany --y
+# set to admin
+drush upwd admin --password=admin
