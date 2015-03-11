f937997218430766f6a648d7128a7e7192b7ec05
Michael Potter <mgp140@gmail.com>
2015-03-11 11:04:49 -0400

    Added front-end dev repo install commands

--- scripts/frontend/setup-frontend-dev.sh
+++ scripts/frontend/setup-frontend-dev.sh
@@ -5,3 +5,9 @@ sudo npm install -g gulp
 sudo npm install -g gulp-hologram
 sudo npm install -g grunt-cli
 sudo gem install bundler
+git clone https://github.com/heyMP/frontend-dev.git
+cd frontend-dev
+sudo npm install
+bower install
+bundle install
+gulp build
