05587c1e8f42e3da7965b7d89e79c884f623d84b
btopro <bto108@psu.edu>
2015-03-12 00:40:40 -0400

    run drush clean up

--- scripts/drupal/setup-drupal.sh
+++ scripts/drupal/setup-drupal.sh
@@ -6,3 +6,4 @@ drush dl drupal-7 --drupal-project-rename=nittany
 drush @nittany si minimal --db-url=mysql://root:@localhost/nittany --account-name=admin --account-mail=admin@nittany.psudug.dev install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --y
 # set to admin
 drush @nittany upwd admin --password=admin
+drush @nittany cleanup
