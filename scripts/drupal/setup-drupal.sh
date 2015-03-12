2d23e1ed9ef3336956bbd074d029349c221216de
btopro <bto108@psu.edu>
2015-03-12 00:59:24 -0400

    setup files directories

--- scripts/drupal/setup-drupal.sh
+++ scripts/drupal/setup-drupal.sh
@@ -12,3 +12,9 @@ drush @nittany cleanup
 sudo cp /vagrant/scripts/server/files/domains.conf /etc/httpd/conf.d/domains.conf
 sudo chkconfig httpd on
 sudo service httpd restart
+# make sure files is owned correctly
+sudo chown -R apache:apache /var/www/html/nittany/sites/default/files
+sudo chmod 775 /var/www/html/nittany/sites/default/files
+# make a private files directory
+sudo mkdir -p /var/www/drupal_priv/nittany
+drush @nittany vset file_private_path /var/www/drupal_priv/nittany
