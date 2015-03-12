10ad01a4e513d7ba32bda13fc7854a6c54ff55c3
btopro <bto108@psu.edu>
2015-03-12 01:17:46 -0400

    git attributes to avoid nastiness in file types across OSs

--- scripts/git/setup-repo.sh
+++ scripts/git/setup-repo.sh
@@ -4,5 +4,6 @@
 cd /var/www/html/nittany
 git init
 cp /vagrant/scripts/git/files/gitignore .gitignore
+cp /vagrant/scripts/git/files/gitattributes .gitattributes
 git add -A
 git commit -m "initial commit"
