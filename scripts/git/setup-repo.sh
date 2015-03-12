266f33b9011b14423f9f7d99a517f52b30c3ddaf
btopro <bto108@psu.edu>
2015-03-12 01:24:51 -0400

    pre-commit.sh taken from elmsln
    which was taken from somewhere else

--- scripts/git/setup-repo.sh
+++ scripts/git/setup-repo.sh
@@ -7,3 +7,5 @@ cp /vagrant/scripts/git/files/gitignore .gitignore
 cp /vagrant/scripts/git/files/gitattributes .gitattributes
 git add -A
 git commit -m "initial commit"
+# add in pre-commit hook for quality control
+cp /vagrant/scripts/git/files/pre-commit.sh .git/hooks/pre-commit
