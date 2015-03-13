465e796d2e6dd913f552467b45c8472df47fd3a8
btopro <bto108@psu.edu>
2015-03-12 23:26:49 -0400

    chown right folder plz

--- scripts/_nonvagrant/newserver.sh
+++ scripts/_nonvagrant/newserver.sh
@@ -5,5 +5,5 @@ git clone https://github.com/psudug/nittany-vagrant /vagrant
 bash /vagrant/scripts/installer/root.sh root
 bash /vagrant/scripts/installer/vagrant.sh
 source .bashrc
-sudo chown apache:apache -R /var/www/html/nittany/default/files/
+sudo chown apache:apache -R /var/www/html/nittany/sites/default/files/
 d sa
