82b3d44de63e78b8ebae7e080fea5679e0f84437
btopro <bto108@psu.edu>
2015-03-12 22:27:23 -0400

    support for an argument to be passed in.. this allows using outside of vagrant... :)

--- scripts/installer/root.sh
+++ scripts/installer/root.sh
@@ -3,5 +3,11 @@
 bash /vagrant/scripts/server/install-system-dependencies.sh
 # apply performance
 bash /vagrant/scripts/server/install-system-performance.sh
+# test for an argument as to what user to write as
+if [ -z $1 ]; then
+    owner='vagrant'
+  else
+    owner=$1
+fi
 chown vagrant:root /var/www/html
 chmod 775 /var/www/html
