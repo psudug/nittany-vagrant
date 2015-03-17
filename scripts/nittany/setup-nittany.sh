0b6c59c0f231c2837f44c9bb6811e4d1877110dc
btopro <bto108@psu.edu>
2015-03-16 22:43:54 -0400

    clean up blocks on admin page manually

--- scripts/nittany/setup-nittany.sh
+++ scripts/nittany/setup-nittany.sh
@@ -6,3 +6,6 @@ git clone https://github.com/psudug/nittany.git drecipes/nittany
 drush cc drush
 # install the baseline of nittany above minimal
 drush @nittany cook nittany_baseline --y
+# clean up blocks on admin page
+drush @nittany sql-query "UPDATE block SET status=0,region=-1 WHERE theme='rubik' AND region='content' AND delta<>'main'"
+drush @nittany cc all
