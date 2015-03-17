293bb0e9d97f52cb33ff4174e973506b8a7faeeb
btopro <bto108@psu.edu>
2015-03-16 23:57:06 -0400

    standard instead of minimal

--- scripts/drupal/setup-drupal.sh
+++ scripts/drupal/setup-drupal.sh
@@ -8,7 +8,7 @@ cd /var/www/html/
 # download drupal
 drush dl drupal-7 --drupal-project-rename=nittany
 # do the initial drush si so we have minimal in place
-drush @nittany si minimal --db-url=mysql://root:@localhost/nittany --account-name=admin --account-mail=admin@nittany.psudug.dev install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --y
+drush @nittany si standard --db-url=mysql://root:@localhost/nittany --account-name=admin --account-mail=admin@nittany.psudug.dev install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --y
 # set to admin
 drush @nittany upwd admin --password=admin
 drush @nittany cleanup
