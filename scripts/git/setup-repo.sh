c4f4fcaa19ec44a46405c4a0ab3da2575a51708c
Bryan Ollendyke <bto108@psu.edu>
2015-03-17 12:06:11 -0400

    Added comments to document everything

--- scripts/git/setup-repo.sh
+++ scripts/git/setup-repo.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/sh
-
+# This script creates a repo of all the work it just did and puts it in version control
+# then it's up to you to hook it up to a git server.
 # setup repo
 cd /var/www/html/nittany
+# initalize the repo
 git init
+# copy our git ignore and attributes goodness
 cp /vagrant/scripts/git/files/gitignore .gitignore
 cp /vagrant/scripts/git/files/gitattributes .gitattributes
+# this adds everything to git
 git add -A
+# commit it all
 git commit -m "initial commit"
 # add in pre-commit hook for quality control
 cp /vagrant/scripts/git/files/pre-commit.sh .git/hooks/pre-commit
