1584f4a9f3b452b221cd9ec22aa4a955671b5dfe
btopro <bto108@psu.edu>
2015-03-16 22:29:55 -0400

    wizard updated to include text editor questions

--- scripts/buckets/wizard.sh
+++ scripts/buckets/wizard.sh
@@ -1,37 +1,79 @@
 #!/bin/bash
-#@todo set up all the recipes to call based on this flow or better yet something more elegant...
+# provide messaging colors for output to console
+txtbld=$(tput bold)             # Bold
+bldgrn=$(tput setaf 2) #  green
+bldred=${txtbld}$(tput setaf 1) #  red
+txtreset=$(tput sgr0)
+dugecho(){
+  echo "${bldgrn}$1${txtreset}"
+}
+dugwarn(){
+  echo "${bldred}$1${txtreset}"
+}
+
 read -p "Would you like a wizard to guide you through this process? (y/n) " WIZ
-if [ "$WIZ" = "n" ]; then
-  touch $HOME/wizard_ran.txt
-  echo "Awesome enjoy building the future! :)"
-  return
-fi
 # lets keep going with everything
 if [ "$WIZ" = "y" ]; then
   touch $HOME/wizard_ran.txt
-  echo "Glad to hear it, let's get started"
-fi
-# ask about SEO
-question="Would you like your site to come equipped with Search Engine Goodness?"
-run='drush -y @nittany cook seo'
-read -p "$question (y/n) " answer
-if [ "$answer" = "y" ]; then
-  $run
+  dugecho "Glad to hear it, let's get started"
+  # ask about SEO
+  question="Would you like your site to come equipped with Search Engine Goodness?"
+  run='drush @nittany cook seo --y'
+  read -p "$question (y/n) " answer
+  if [ "$answer" = "y" ]; then
+    $run
+    # issue with directory ownership run from here
+    sudo chown -R $USER:apache /var/www/html/nittany/sites/default/files
+  else
+    dugwarn "To run this in the future you can issue: $run"
+  fi
+
+  # ask about wysiwyg
+  question="Would you like your site to come with CKEditor 4.x setup already?"
+  run='drush @nittany cook textbook --y'
+  read -p "$question (y/n) " answer
+  if [ "$answer" = "y" ]; then
+    # we need to get textbook repos before running
+    git clone https://github.com/psudug/quail4textbook.git /var/www/html/nittany/sites/all/libraries/quail
+    git clone https://github.com/psudug/ckeditor4textbook.git /var/www/html/nittany/sites/all/libraries/ckeditor
+    $run
+  else
+    dugwarn "To run this in the future you can issue: $run"
+  fi
+
+  # ask about media handling
+  #question="Would you like your site to come equipped with our recommended media handling?"
+  #run='drush -y @nittany cook nittany_media'
+  #read -p "$question (y/n) " answer
+  #if [ "$answer" = "y" ]; then
+  #  $run
+  #else
+  #  dugwarn "run this in the future: $run"
+  #fi
+
+  # ask about theme
+  #drush @nittany cook nittany_theme --skip-confirm
 else
-  echo "run this in the future: $run"
+  dugecho "Awesome enjoy building the future! :)"
 fi
+# file existing means this won't execute on ssh login
+touch $HOME/wizard_ran.txt
+# clear caches so everyone is happy
+drush @nittany cc all
+# print status so they know it lives
+drush @nittany status
+dugecho "If you ever want to run through this wizard again you can either delete ~/wizard_ran.txt or run: bash /vagrant/scripts/buckets/wizard.sh"
+dugecho "That's all for now but trust us there's more to come... happy drupaling!"
+dugecho ""
+dugecho "http://nittany.psudug.dev/"
+dugecho "Login: admin / admin"
+dugecho ""
+dugecho "WE ARE"
+dugecho ":::::::::   ::::::::  :::    :::      :::::::::  :::    :::  ::::::::  "
+dugecho ":+:    :+: :+:    :+: :+:    :+:      :+:    :+: :+:    :+: :+:    :+: "
+dugecho "+:+    +:+ +:+        +:+    +:+      +:+    +:+ +:+    +:+ +:+        "
+dugecho "+#++:++#+  +#++:++#++ +#+    +:+      +#+    +:+ +#+    +:+ :#:        "
+dugecho "+#+               +#+ +#+    +#+      +#+    +#+ +#+    +#+ +#+   +#+# "
+dugecho "#+#        #+#    #+# #+#    #+#      #+#    #+# #+#    #+# #+#    #+# "
+dugecho "###         ########   ########       #########   ########   ########  "
 
-# ask about media handling
-#question="Would you like your site to come equipped with our recommended media handling?"
-#run='drush -y @nittany cook nittany_media'
-#read -p "$question (y/n) " answer
-#if [ "$answer" = "y" ]; then
-#  $run
-#else
-#  echo "run this in the future: $run"
-#fi
-
-# ask about theme
-#drush @nittany cook nittany_theme --skip-confirm
-
-echo "that's all for now but trust us there's more to come... happy drupaling!"
