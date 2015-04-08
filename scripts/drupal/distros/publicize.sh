9cea768634e6b94f09a74a89e2bd758155d248bf
btopro <bto108@psu.edu>
2015-04-08 13:37:21 -0400

    avoid minor notice

--- scripts/drupal/distros/publicize.sh
+++ scripts/drupal/distros/publicize.sh
@@ -9,7 +9,7 @@ cd nittany
 rm -rf modules
 rm -rf themes
 rm -rf .git
-find . -not -name '*.make*' | xargs rm
+find . -type f -not -name '*.make*' | xargs rm
 # build off the make files
 drush make local.make.example --y
 drush @nittany si publicize -y --db-url=mysql://root@localhost/nittany --account-name=admin --account-mail=admin@nittany.psudug.dev install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --y
