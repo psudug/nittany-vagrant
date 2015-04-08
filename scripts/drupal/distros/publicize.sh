69ce9b22e369531a58124364b4d53988ea84f132
btopro <bto108@psu.edu>
2015-04-08 13:45:04 -0400

    clean up stupid menu block title thing

--- scripts/drupal/distros/publicize.sh
+++ scripts/drupal/distros/publicize.sh
@@ -12,5 +12,8 @@ rm -rf .git
 find . -type f -not -name '*.make*' | xargs rm
 # build off the make files
 drush make local.make.example --y
-drush @nittany si publicize -y --db-url=mysql://root@localhost/nittany --account-name=admin --account-mail=admin@nittany.psudug.dev install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --y
+# clean up make file now that we built
 rm *.make*
+drush @nittany si publicize -y --db-url=mysql://root@localhost/nittany --account-name=admin --account-mail=admin@nittany.psudug.dev install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --y
+# account for STUPID caching issue of menu name
+drush @nittany sqlq "UPDATE block SET title='<none>' WHERE module='menu_block'"
