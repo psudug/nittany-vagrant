b8975ae815c5c4d685e7460073e7c00052b2d6b2
btopro <bto108@psu.edu>
2015-04-13 12:47:45 -0400

    make sure these are turned on, they have been disabled at times for some reason

--- scripts/drupal/distros/2-publicize.sh
+++ scripts/drupal/distros/2-publicize.sh
@@ -15,5 +15,7 @@ drush make local.make.example --y
 # clean up make file now that we built
 rm *.make*
 drush @nittany si publicize -y --db-url=mysql://root@localhost/nittany --account-name=admin --account-mail=$1 install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --y
+drush @nittany en publicize_defaults --y
+drush @nittany en publicize_content --y
 # account for STUPID caching issue of menu name
 drush @nittany sqlq "UPDATE block SET title='<none>' WHERE module='menu_block'"
