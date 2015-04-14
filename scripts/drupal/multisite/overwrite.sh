88a4958d73b4b4ef161f35431a02353c1d09f432
jpat14 <jpat14@2870565.no-reply.drupal.org>
2015-04-14 08:39:53 -0400

    Overwrite script bug fixes

--- scripts/drupal/multisite/overwrite.sh
+++ scripts/drupal/multisite/overwrite.sh
@@ -8,9 +8,9 @@ dugwarn(){
 
 #perform overwrite preconfig
 cd /var/www/html
-sitelist=$(find . -maxdepth 1 -type d)
+clear
+sitelist=$(find . ! -path . -maxdepth 1 -printf '%T\n' -type d)
 PS3="Type a number or 'q' to quit: "
-select site in $sitelist; do
   dugwarn "You are about to recursively remove your site located in /var/www/html/$site. This will be permanant!"
   read "Are you sure you wish to do this? " confirm
   if [[ confirm == "y" ]]; then
