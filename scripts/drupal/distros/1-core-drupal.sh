cb077f43ebae9957f5e3e75f200de898be6f1d1c
btopro <bto108@psu.edu>
2015-04-15 12:56:43 -0400

    mail setting

--- scripts/drupal/distros/1-core-drupal.sh
+++ scripts/drupal/distros/1-core-drupal.sh
@@ -3,4 +3,4 @@
 # download drupal
 drush dl drupal-7 --drupal-project-rename=nittany
 # do the initial drush si so we have minimal in place
-drush @nittany si standard --db-url=mysql://root:@localhost/nittany --account-name=admin --account-mail=$email install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --y
+drush @nittany si standard  -y --db-url=mysql://root@localhost/nittany --account-name=admin --account-mail=$1 install_configure_form.update_status_module='array(FALSE,FALSE)' install_configure_form.site_default_country=US --y
