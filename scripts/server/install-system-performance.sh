ad5c9b721598ef10ab6c37f64eeeee6fd368c015
Brad A Fisher Jr <baf139@psu.edu>
2015-04-24 13:53:49 -0400

    compartmentalizing the performance configuration fixes #40

--- scripts/server/install-system-performance.sh
+++ scripts/server/install-system-performance.sh
@@ -2,19 +2,21 @@
 # where am i? move to where I am. This ensures source is properly sourced
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+sudo mkdir -p /etc/httpd/conf.d/web_performance
+
 # defaults
 wwwuser='apache'
 apcini="/etc/php.d/apc.ini"
 phpini="/etc/php.ini"
 mycnf="/etc/my.cnf"
-zzz_performance="/etc/httpd/conf.d/zzz_performance.conf"
+web_performance="/etc/httpd/conf.d/web_performance/"
 
 # performance / recommended settings
 rm $apcini
 cp files/apc.txt $apcini
 cat files/php.txt >> $phpini
 cat files/my.txt >> $mycnf
-cp files/zzz_performance.conf $zzz_performance
+cp files/web_performance/* -rf $web_performance
 
 chkconfig httpd on
 chkconfig mysqld on
